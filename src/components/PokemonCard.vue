5730f84771e6cdf06d1c07fde153ec12d9293da8
Martin Pilaszek <77743301+mapszk@users.noreply.github.com>
2025-03-04 20:26:47 -0300

    Created list

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps(['id', 'name'])
+const baseImageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+</script>
+
+<template>
+  <router-link
+    class="rounded-lg bg-blue-500 p-4 block flex flex-col gap-4 hover:scale-[1.05] transition-transform relative"
+  >
+    <picture class="block aspect-square size-full border border-yellow-400 rounded-lg">
+      <img
+        :src="baseImageUrl + props.id + '.png'"
+        :alt="props.name + 'image'"
+        class="object-contain size-full"
+      />
+    </picture>
+    <button class="rounded-full size-8 bg-yellow-500 absolute top-8 right-8">+</button>
+    <span class="bg-white px-4 py-2 rounded-lg capitalize text-lg font-medium">{{
+      props.name
+    }}</span>
+  </router-link>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,26 @@
 <script setup>
 import { useGetData } from '@/hooks/useFetchData'
 import { endpoints } from '@/services/api'
-import { toRaw, watch } from 'vue'
+import PokemonCard from '@/components/PokemonCard.vue'
 
 const { isLoading, data } = useGetData({
   queryKey: ['pokemonsList'],
-  url: endpoints.pokemons(),
+  url: endpoints.pokemons('limit=12'),
 })
 
-watch(data, (newData) => {
-  if (newData) console.log('DATA LOADED', toRaw(newData))
-})
+const getPokemonId = (url) => url.split('/').filter(Boolean).pop()
 </script>
 
 <template>
   <main class="container">
-    <h1 class="text-4xl">Pokemones</h1>
-    <div v-if="!isLoading">
-      <span v-for="pokemon in data.results" :key="pokemon.id">{{ pokemon.name }}</span>
+    <h1 class="text-4xl font-semibold">Pokemones</h1>
+    <div v-if="!isLoading" class="grid grid-cols-3 gap-5 py-12 pt-8">
+      <PokemonCard
+        v-for="pokemon in data.results"
+        :key="pokemon.name"
+        :name="pokemon.name"
+        :id="getPokemonId(pokemon.url)"
+      ></PokemonCard>
     </div>
   </main>
 </template>
